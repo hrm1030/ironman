e2d167ef323cc25b0946ca3c7a5d5b6b02e54037
lovecoding1987 <hrm.2021@outlook.com>
2021-05-18 21:16:28 -0700

    update

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -8,9 +8,6 @@ class CategoryController extends Controller {
     
     public function index()
     {
-        // $recent_reivews = Review::orderBy('regarding', 'desc')->limit(10)->get();
-        // // die(print_r($recent_reivews));
-        // return view('pages.category')->with('recent_reviews', $recent_reivews);
         $categories = array();
         $pcategories = Category::where('pid', 1)->orderBy('order_num', 'asc')->get();
         foreach( $pcategories as $pcategory ) {
@@ -21,8 +18,4 @@ class CategoryController extends Controller {
         return view('pages.category.category_list')->with('categories', $categories);
     }
 
-    public function detail(Request $request)
-    {
-
-    }
 }
